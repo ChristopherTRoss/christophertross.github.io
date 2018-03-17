15b521a1dac5c869ade56899591cc144175b7a59
Christopher Ross <cross22@lsu.edu>
2018-03-16 21:50:09 -0500

    Added nav bar and smooth scrolling

--- Website.js
+++ Website.js
@@ -1,24 +1,40 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+       
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('link');
+        })
+        $(this).addClass('link');
+      
+        var target = this.hash,
+        menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 45
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
 });
+
+//Underlines tab on navbar based on psoiton on site
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#menu-center a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top- 45 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#menu-center a').removeClass("link");
+            currLink.addClass("link");
+        }
+        else{
+            currLink.removeClass("link");
+        }
+    });
+}
