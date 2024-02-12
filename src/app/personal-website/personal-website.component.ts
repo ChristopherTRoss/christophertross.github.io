d4539281b37f68cd8e3b70263a0a09ce5483b984
Christopher Ross <christophertross1@gmail.com>
2024-02-12 16:29:01 -0600

    cleaning up projects section

--- src/app/personal-website/personal-website.component.ts
+++ src/app/personal-website/personal-website.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-personal-website',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './personal-website.component.html',
   styleUrl: './personal-website.component.css'
 })
