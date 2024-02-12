c65b2c288b644d2123483dc246ebb3fb6a93a41e
Christopher Ross <christophertross1@gmail.com>
2024-02-12 11:37:18 -0600

    cleaning up imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonalWebsiteComponent } from './personal-website/personal-website.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PersonalWebsiteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
