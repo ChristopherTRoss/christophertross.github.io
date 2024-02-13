a334b0c79f4b835c5cdc57cdee6ef6eabf53b17c
Christopher Ross <christophertross1@gmail.com>
2024-02-12 20:28:07 -0600

    Adding support for gh pages

--- src/assets/js/scroll-logic.js
+++ src/assets/js/scroll-logic.js
@@ -1,25 +1,5 @@
 $(document).ready(function () {
     $(document).on("scroll", onScroll);
-    //Smooth scroll
-    $('a[href^="#"]').on('click', function (e) {
-       
-        $(document).off("scroll");
-        
-        $('a').each(function () {
-            $(this).removeClass('link');
-        })
-        $(this).addClass('link');
-      
-        var target = this.hash,
-        menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top - 60
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
 });
 
 //Underlines tab on navbar based on position on site
