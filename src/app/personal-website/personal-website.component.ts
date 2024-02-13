d03614ca35c6b2b3a0ce115e8d92bb7f13120a93
Christopher Ross <christophertross1@gmail.com>
2024-02-13 16:15:53 -0600

    styling cleanup

--- src/app/personal-website/personal-website.component.ts
+++ src/app/personal-website/personal-website.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from "@angular/material/icon";
+
 
 @Component({
   selector: 'app-personal-website',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatIconModule],
   templateUrl: './personal-website.component.html',
   styleUrl: './personal-website.component.css'
 })
