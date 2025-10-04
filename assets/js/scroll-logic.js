bca40ad654702de6a5b3de5287c1247666e99dc1
Chris <christophertross1@gmail.com>
2025-10-04 12:49:40 -0600

    Auto-generated commit

--- assets/js/scroll-logic.js
+++ assets/js/scroll-logic.js
@@ -3,17 +3,27 @@ $(document).ready(function () {
 });
 
 //Underlines tab on navbar based on position on site
-function onScroll(event){
+function onScroll(event) {
     var scrollPos = $(document).scrollTop();
     $('#menu-right a').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
-        if (refElement.position().top- 60 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+        if (refElement.position().top - 60 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
             $('#menu-right a').removeClass("link");
             currLink.addClass("link");
         }
-        else{
+        else {
             currLink.removeClass("link");
         }
     });
 }
+
+window.addEventListener('scroll', function () {
+    const nav = document.getElementById('nav');
+    if (window.scrollY > 50) { // when user scrolls down
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
